20e46dc1c17cdb2d4f6fb09302f27a43c2884c7c
HUSTERGS <1148690954@qq.com>
2020-04-03 21:16:47 +0800

    :zap: Improve 201809-4 from 50 to 70
    找到了远古代码,写的比现在分数高,惊了

--- 201809/201809-4.py
+++ 201809/201809-4.py
@@ -1,59 +1,63 @@
-from sys import stdin
+# score 70
+# 远古代码
+import sys
 
-n = int(stdin.readline().strip())
-nums = tuple(int(x) for x in stdin.readline().strip().split())
-# 第一天价格的上界
-bound = [0, ] * n
-for i in range(2, n - 2):
-    bound[i] = 3 * min(nums[i - 1], nums[i], nums[i + 1])
-bound[0] = min(2 * nums[0], 3 * nums[1])
-if n >= 3:
-    bound[1] = min(2 * nums[0], 3 * nums[1], 3 * nums[2])
-    bound[n - 2] = min(2 * nums[n - 1], 3 * nums[n - 2], 3 * nums[n - 3])
-bound[n - 1] = min(2 * nums[n - 1], 3 * nums[n - 2])
+num = int(sys.stdin.readline())
+prices = [int(price) for price in sys.stdin.readline().split()]
 
-result = [0, ] * n
+result = []
 
 
-def determine_i(i):
-    global result
-    if i == 0:
-        for a0 in range(1, bound[0] + 1):
-            result[0] = a0
-            if determine_i(i + 1) is not None:
-                return result
-    elif i == 1:
-        for bias in range(2):
-            a1 = 2 * nums[0] + bias - result[0]
-            result[1] = a1
-            if a1 > bound[1]:
-                return None
-            if n == 2:
-                return result
-            if determine_i(i + 1) is not None:
-                return result
-        return None
-    elif i == n - 1:
-        for bias in range(2):
-            an_1 = 2 * nums[n - 1] - result[n - 2] + bias
-            result[n - 1] = an_1
-            if an_1 > bound[n-1]:
-                return None
-            if 1 <= an_1 <= bound[n - 1]:
-                if n >= 3 and (result[n - 3] + result[n - 2] + result[n - 1]) // 3 == nums[n - 2]:
-                    return result
-        return None
-
-    else:
-        for bias in range(3):
-            ai = 3 * nums[i-1] - result[i - 1] - result[i - 2] + bias
-            result[i] = ai
-            if ai > bound[i] or ai <= -2:
-                return None
-            if 1 <= ai <= bound[i] and determine_i(i + 1) is not None:
-                return result
-        return None
+def recursive(order, a_one, a_two):
+    if order == num - 1:
+        if (a_one + a_two) // 2 == prices[-1]:
+            return True
+        else:
+            return False
+    possible_one = 3 * prices[order] - a_one - a_two
+    if possible_one > 0:
+        if (recursive(order + 1, a_two, possible_one)):
+            result.append(possible_one)
+            return True
+        else:
+            pass
+    if possible_one > -1:
+        if (recursive(order + 1, a_two, possible_one + 1)):
+            result.append(possible_one + 1)
+            return True
+        else:
+            pass
+    if possible_one > -2:
+        if (recursive(order + 1, a_two, possible_one + 2)):
+            result.append(possible_one + 2)
+            return True
+        else:
+            pass
+    return False
 
 
-determine_i(0)
-print(" ".join([str(x) for x in result]))
+def start():
+    for i in range(1, prices[0] * 2):
+        a_two = prices[0] * 2 - i
+        result.extend([i, a_two])
+        if (i + a_two) // 2 == prices[0]:
+            if recursive(1, i, a_two):
+                return True
+            else:
+                result.pop()
+                result.pop()
+        elif (i + a_two + 1) // 2 == prices[0]:
+            reuslt.extend([i, a_two + 1])
+            if recursive(1, i, a_two + 1):
+                return True
+        else:
+            continue
+
+
+start()
+a = result[0:2]
+b = result[2:]
+b.reverse()
+a.extend(b)
+print(' '.join([str(num) for num in a]))
+
