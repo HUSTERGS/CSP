cfaf0732ee880a8c591fc9b173d6ce357704ce2d
HUSTERGS <1148690954@qq.com>
2020-08-22 03:42:32 +0800

    :white_check_mark: try to improve 201512-3 with deque,still timeout

--- 201512/201512-3.py
+++ 201512/201512-3.py
@@ -2,31 +2,33 @@
 # 使用了各种办法还是无法提高到100，一直超时
 from sys import stdin
 from collections import deque
+import sys
 
-m, n, q = [int(x) for x in stdin.readline().split()]
+m, n, q = [int(x) for x in stdin.readline().strip().split()]
 canvas = [[".", ] * m for _ in range(n)]
-
+sys.setrecursionlimit(100000)
 queue = deque()
 handled = set()
 
 
 def filling(x, y, c):
-    if 0 <= x < m and 0 <= y < n:
-        if canvas[y][x] != "-" and canvas[y][x] != "|" and canvas[y][x] != "+":
-            canvas[y][x] = c
-            handled.add((x, y))
-            if (x, y + 1) not in handled:
-                filling(x, y + 1, c)
-            if (x, y - 1) not in handled:
-                filling(x, y - 1, c)
-            if (x + 1, y) not in handled:
-                filling(x + 1, y, c)
-            if (x - 1, y) not in handled:
-                filling(x - 1, y, c)
+    queue.append((x, y))
+    while len(queue) != 0:
+        x, y = queue.popleft()
+        canvas[y][x] = c
+        handled.add((x, y))
+        if (x, y + 1) not in handled and 0 <= x < m and 0 <= y + 1 < n and canvas[y + 1][x] not in "-|+":
+            queue.append((x, y + 1))
+        if (x, y - 1) not in handled and 0 <= x < m and 0 <= y - 1 < n and canvas[y - 1][x] not in "-|+":
+            queue.append((x, y - 1))
+        if (x + 1, y) not in handled and 0 <= x + 1 < m and 0 <= y < n and canvas[y][x + 1] not in "-|+":
+            queue.append((x + 1, y))
+        if (x - 1, y) not in handled and 0 <= x - 1 < m and 0 <= y < n and canvas[y][x - 1] not in "-|+":
+            queue.append((x - 1, y))
 
 
 for i in range(q):
-    string = stdin.readline().split()
+    string = stdin.readline().strip().split()
     if len(string) == 4:
         # 操作1 填充操作
         _, x, y, c = string
@@ -41,14 +43,8 @@ for i in range(q):
             # 纵向的线段
             y1 = n - y1 - 1
             y2 = n - y2 - 1
-            if y1 > y2:
-                upper_bound = y1
-                lower_bound = y2
-            else:
-                upper_bound = y2
-                lower_bound = y1
-            # upper_bound = max(y1, y2)
-            # lower_bound = min(y1, y2)
+            upper_bound = max(y1, y2)
+            lower_bound = min(y1, y2)
             for y in range(lower_bound, upper_bound + 1):
                 if canvas[y][x1] == "-" or canvas[y][x1] == "+":
                     canvas[y][x1] = "+"
@@ -57,14 +53,8 @@ for i in range(q):
         else:
             # 横向的线段
             y = n - y1 - 1
-            if x1 > x2:
-                upper_bound = x1
-                lower_bound = x2
-            else:
-                upper_bound = x2
-                lower_bound = x1
-            # upper_bound = max(x1, x2)
-            # lower_bound = min(x1, x2)
+            upper_bound = max(x1, x2)
+            lower_bound = min(x1, x2)
             for x in range(lower_bound, upper_bound + 1):
                 if canvas[y][x] == "|" or canvas[y][x] == "+":
                     canvas[y][x] = "+"
